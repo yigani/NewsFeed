9d79af7420ed1bbad48eba2a657993161e9304bc
deabaind <deabaind@gmail.com>
2025-04-08 11:33:45 +0900

    프로젝트 파일 생성

---
+++ src/main/java/com/example/NewsFeed/NewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewsFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NewsFeed/NewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewsFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
