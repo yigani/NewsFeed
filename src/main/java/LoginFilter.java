9e10e427b6f81e3d95eaf07462bed5ac03920f7b
soyoonp <soyoonp@naver.com>
2025-04-10 21:43:44 +0900

    TEST

---
+++ src/main/java/LoginFilter.java
@@ -0,0 +1,58 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+    //모든 클래스는 필터를 타게 됨
+    private static final String[] WHITE_LIST = {"/", "/users/signup", "/users/login", "users/logout"};
+    //위 주소 빼고 나머지 다 필터링
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+
+        // 다양한 기능을 사용하기 위해 다운 캐스팅
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        // 다양한 기능을 사용하기 위해 다운 캐스팅
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // 로그인을 체크 해야하는 URL인지 검사
+
+        if (!isWhiteList(requestURI)) {
+
+            HttpSession session = httpRequest.getSession(false);
+
+            // 로그인하지 않은 사용자인 경우
+            if (session == null || session.getAttribute("sessionKey값") == null) {
+                throw new RuntimeException("로그인 해주세요.");
+
+            }
+            return;
+            // 로그인 성공 로직
+
+        }
+
+        // 다음 필터 없으면 Servlet -> Controller 호출
+        chain.doFilter(request, response);
+
+
+    }
+
+    // 로그인 여부를 확인하는 URL인지 체크하는 메서드
+    private boolean isWhiteList(String requestURI) {
+        // request URI가 whiteListURL에 포함되는지 확인
+        // 포함되면 true 반환
+        // 포함되지 않으면 false 반환
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
