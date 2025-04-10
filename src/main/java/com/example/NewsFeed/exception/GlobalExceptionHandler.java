72b408f9b15c264831d90f551638444a00d39d1b
deabaind <deabaind@gmail.com>
2025-04-10 14:47:40 +0900

    fix: Json 형태로 출력할 수 있게 수정

--- src/main/java/com/example/NewsFeed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/NewsFeed/exception/GlobalExceptionHandler.java
@@ -12,21 +12,17 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-//    // 유효성 검증 오류 예외 처리
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<String> notBlankException(MethodArgumentNotValidException exception) {
-//        String exceptionMessage = exception
-//                // 검증 오류 전체 정보를 가져온다.
-//                .getBindingResult()
-//                // 모든 오류를 리스트로 반환한다.
-//                .getAllErrors()
-//                // 가장 첫 번째 오류만 가져온다.
-//                .get(0)
-//                // 어노테이션에 설정한 메세지가 출력된다.
-//                .getDefaultMessage();
-//        Map<String, String> errorMessageList = new HashMap<>() {
-//            String errorMessage = exception.getBindingResult().getFieldErrors().forEach(error);
-//        };
-//        return new ResponseEntity<>(exceptionMessage, HttpStatus.BAD_REQUEST);
-//    }
+    // 유효성 검증 오류 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> notBlankException(MethodArgumentNotValidException exception) {
+        // json 형태로 반환할 Map 생성
+        Map<String, String> errorResponse = new HashMap<>();
+        // 예외 객체에서 바인딩한 결과를 필드 단위로 가져온다
+        exception.getBindingResult().getFieldErrors()
+                .forEach(error -> {
+                    // 필드와 메세지를 꺼내서 Map에 넣기
+                    errorResponse.put(error.getField(), error.getDefaultMessage());
+                });
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 }
