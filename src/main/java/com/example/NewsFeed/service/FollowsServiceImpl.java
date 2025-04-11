166a840981acd3d14dd3ae2120921360b1adb5ab
isom <smallsol.fa@gmail.com>
2025-04-11 10:12:47 +0900

    feat: 팔로잉 팔로워 이름 중복으로 뜨던것 수정

--- src/main/java/com/example/NewsFeed/service/FollowsServiceImpl.java
+++ src/main/java/com/example/NewsFeed/service/FollowsServiceImpl.java
@@ -4,10 +4,13 @@ import com.example.NewsFeed.entity.Follows;
 import com.example.NewsFeed.entity.Users;
 import com.example.NewsFeed.repository.FollowsRepository;
 import com.example.NewsFeed.repository.UsersRepository;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class FollowsServiceImpl implements FollowsService{
@@ -21,37 +24,44 @@ public class FollowsServiceImpl implements FollowsService{
         this.followsRepository = followsRepository;
     }
 
+    // 이름 중복문제 해결해야함...
+    //팔로잉 유저목록
     @Override
     public List<String> followingUserNames(Long userId) {
 
         Users user = usersRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("팔로잉한 유저가 없습니다."));
 
-        List<Follows> following = followsRepository.findByFollowing(user);
+        List<Follows> following = followsRepository.findByFollower(user);
+        Set<Long> names = new HashSet<>();
         List<String> followingUserName = new ArrayList<>();
 
         for (Follows follow : following){
-            //
             Users followingUser = follow.getFollowing();
-            followingUserName.add(followingUser.getUserName());
+            if(names.add(followingUser.getId())){
+                followingUserName.add(followingUser.getUserName());
+            }
         }
 
         return followingUserName;
     }
 
-    //수정필요
+    //팔로워 유저 목록
     @Override
     public List<String> followerUserNames(Long userId) {
 
         Users user = usersRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("팔로워한 유저가 없습니다."));
 
-        List<Follows> follower = followsRepository.findByFollower(user);
+        List<Follows> follower = followsRepository.findByFollowing(user);
+        Set<Long> names = new HashSet<>();
         List<String> followerUserName = new ArrayList<>();
 
         for (Follows follow : follower){
-            Users followergUser = follow.getFollower();
-            followerUserName.add(followergUser.getUserName());
+            Users followerUser = follow.getFollower();
+            if(names.add(follow.getId())){
+                followerUserName.add(followerUser.getUserName());
+            }
         }
         return followerUserName;
     }
