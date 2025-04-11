cff636f3eb6d1442651ff64d70cec5cad9ee9698
Eunji Lee <aggee212@gmail.com>
2025-04-11 16:41:37 +0900

    feat: 성별 enum으로 관리

--- src/main/java/com/example/NewsFeed/config/Gender.java
+++ src/main/java/com/example/NewsFeed/config/Gender.java
@@ -1,4 +1,16 @@
 package com.example.NewsFeed.config;
 
 public enum Gender {
+   M, F;
+
+   public static Gender fromInt(int gender) {
+       if (gender == 1) {
+           return M;
+       } else if (gender == 2){
+           return F;
+       } else {
+           throw new IllegalArgumentException("성별은 1 또는 2만 가능합니다.");
+       }
+   }
+
 }
